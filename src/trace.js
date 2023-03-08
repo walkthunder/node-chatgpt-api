3c5e89686ff8f82f3932315b5691ff6a937008d5
walkthunder Cooper <walkthunder@163.com>
2023-03-08 22:16:52 +0800

    feat(trace): Output trace response for debug.

--- src/trace.js
+++ src/trace.js
@@ -1,29 +1,32 @@
 // TODO: List all event name
+// eslint-disable-next-line import/prefer-default-export
 export const trace = async (evtName, params) => {
-    if(process.env.DEBUG) {
+    if (process.env.DEBUG) {
         return;
     }
-    return fetch(`https://www.google-analytics.com/mp/collect?api_secret=hrruIOu_Q5yOrM1__pW4dA&measurement_id=G-JETSEF2PMM`, {
+    // eslint-disable-next-line consistent-return
+    return fetch('https://www.google-analytics.com/mp/collect?api_secret=hrruIOu_Q5yOrM1__pW4dA&measurement_id=G-JETSEF2PMM', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'user-agent': 'chat-node-server'
+            'user-agent': 'chat-node-server',
         },
         body: JSON.stringify({
-            'non_personalized_ads': true,
+            non_personalized_ads: true,
             events: [
                 {
                     name: evtName,
-                    ...(params|| {})
-                }
-            ]
+                    ...(params || {}),
+                },
+            ],
         }),
     })
-    .then(resp => {
-        console.log('trace done');
-        return resp;
-    })
-    .catch(err => {
-        console.error('Trace exception caught: ', evtName, err);
-    });
-}
+        .then(resp => resp.json())
+        .then((resp) => {
+            console.log('trace done', resp);
+            return resp;
+        })
+        .catch((err) => {
+            console.error('Trace exception caught: ', evtName, err);
+        });
+};
