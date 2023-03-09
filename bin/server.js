49a7bfbdc8e85722327617d43a8dc74434e9708c
walkthunder Cooper <walkthunder@163.com>
2023-03-09 21:57:07 +0800

    feat(chat): Revert auth check.

--- bin/server.js
+++ bin/server.js
@@ -134,29 +134,29 @@ server.post('/api/usage', async (request, reply) => {
 
 server.post('/api/chat', async (request, reply) => {
     console.log('api chat message - ', JSON.stringify(request.body));
-    // try {
-    //     const { hash } = request.body || {};
-    //     if (!hash) {
-    //         throw new Error('Not Authorized');
-    //     }
-    //     console.log('hash and salt: ', hash);
-    //     const bytes = CryptoJS.AES.decrypt(hash, process.env.CHAT_SALT);
-    //     console.log('request decrypt: ', bytes);
-    //     const {
-    //         id, openId, left, date,
-    //     } = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    //     console.log('request hash data: ', id, openId, left, date);
-    //     if (!id || !openId || (left <= 0)) {
-    //         throw new Error('Invalid Hash Data');
-    //     }
-    //     if (Math.abs(new Date().valueOf() - Number(date)) > 20000) {
-    //         throw new Error('Outdated Request');
-    //     }
-    //     // Continue biz
-    // } catch (error) {
-    //     reply.code(400).send(error?.message || 'Auth Failed');
-    //     return;
-    // }
+    try {
+        const { hash } = request.body || {};
+        if (!hash) {
+            throw new Error('Not Authorized');
+        }
+        console.log('hash and salt: ', hash);
+        const bytes = CryptoJS.AES.decrypt(hash, process.env.CHAT_SALT);
+        console.log('request decrypt: ', bytes);
+        const {
+            id, openId, left, date,
+        } = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        console.log('request hash data: ', id, openId, left, date);
+        if (!id || !openId || (left <= 0)) {
+            throw new Error('Invalid Hash Data');
+        }
+        if (Math.abs(new Date().valueOf() - Number(date)) > 20000) {
+            throw new Error('Outdated Request');
+        }
+        // Continue biz
+    } catch (error) {
+        reply.code(400).send(error?.message || 'Auth Failed');
+        return;
+    }
 
     const body = request.body || {};
     const abortController = new AbortController();
