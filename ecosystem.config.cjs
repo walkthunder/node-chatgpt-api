42a79e2ef008f8de5d677d99d855e33e056523c9
walkthunder Cooper <walkthunder@163.com>
2023-12-18 12:46:24 +0800

    conf(nginx): Add nginx config for chat api.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
         env: {
             NODE_ENV: 'production',
             API_PORT: 6001,
+            API_HOST: '0.0.0.0',
             UPSTREAM: 'https://api.openai-proxy.org/v1/chat/completions',
             OPENAI_MODEL: 'gpt-3.5-turbo',
         },
